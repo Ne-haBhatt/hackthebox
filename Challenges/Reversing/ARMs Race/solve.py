f53c8a342cc8c8c25262527c2269d1ffdcdd9846
Neha  Bhatt <101342579+nehabhatt1503@users.noreply.github.com>
2024-11-16 07:42:20 +0530

    Update solve.py

--- Challenges/Reversing/ARMs Race/solve.py
+++ Challenges/Reversing/ARMs Race/solve.py
@@ -1,21 +1,37 @@
-"""def b(name):2221
-    print("Hi",+name)
+from unicorn import *
+from unicorn.arm_const import *
+from pwn import *
 
-"""
-    
+SERVER = "94.241.45.123"
+PORT = 47050
 
-fact=1
+def emulate_arm(hex_code):
+    uc = Uc(UC_ARCH_ARM, UC_MODE_ARM)
 
-a=int(input("enter a number "))
-if a<0:
-    print("factorial does not exist")
-elif a==0:
-    print('factorial exist')
+    ADDRESS = 0x1000000
 
-else:
-    for i in range(1,a+1):
-        fact=fact*i
-    print('factorial of',a,'is',fact)    
-    
-    
-    
+    uc.mem_map(ADDRESS, 2 * 1024 * 1024)
+    uc.mem_write(ADDRESS, hex_code)
+
+    try:
+        uc.emu_start(ADDRESS, ADDRESS + len(hex_code))
+    except UcError as e:
+        print(f"Error: {e}")
+
+    r0_value = uc.reg_read(UC_ARM_REG_R0)
+    return r0_value
+
+def ConnectServer():
+    p = remote(SERVER, PORT)
+
+    for i in range(50):
+        level = p.recvuntil(": ")
+        hex_code = p.recvuntil("Register r0:").decode().split("\n")[0]
+
+        r0_result = emulate_arm(bytes.fromhex(hex_code))
+        print(f"Level: {i+1}, The R0 result: {str(r0_result)}")
+
+        p.sendline(str(r0_result).encode())
+    p.interactive()
+
+ConnectServer()
